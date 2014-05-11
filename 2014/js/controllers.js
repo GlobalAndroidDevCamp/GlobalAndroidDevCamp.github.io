1143b2d6de396e3830ae20f5fc6daed80f140b5f
Friedger Müffke <friedger@gmail.com>
2014-05-11 16:01:50 +0200

    Update controllers.js

--- 2014/js/controllers.js
+++ 2014/js/controllers.js
@@ -3,7 +3,7 @@
 /* Controllers */
 
 angular.module('gadc.controllers', []).
-  controller('CitiesCtrl', ['$scope', '$http', function($scope) {
+  controller('CitiesCtrl', ['$scope', '$http', function($scope, $http) {
   	$scope.cities = [{code:'berlin', name:'Berlin'},{code:'brussels', name:'Brussels'}, {code:'duesseldorf', name:'Düsseldorf'}];
 	$http.get('city/all.json').success(function(data) {
       		$scope.cities = data;
